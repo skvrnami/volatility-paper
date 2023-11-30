c49b2b4f3213aa2746f0881681bbd34d30976d3a
Michael Škvrňák <michael.skvrnak@protonmail.com>
2023-11-30 11:46:56 +0100

    Add charts to appendix

--- charts.R
+++ charts.R
@@ -755,3 +755,349 @@ ggsave("figs/generations5_all_2nd_gen.png",
        dpi = 300,
        type = "cairo")
 
+# Appendix -----------------------------------------------
+np_share_hist_en <- 
+    final_data %>% 
+    select(country_name_short, election_year, contains("np_share")) %>% 
+    pivot_longer(cols = c(starts_with("np_"))) %>% 
+    filter(!is.na(value)) %>%
+    mutate(name = case_when(name == "np_share_cv_1" ~ "Genuinely new parties, 1%",
+                            name == "np_share_pnp_1" ~ "Partially new parties, 1%",
+                            name == "np_share_nc_1" ~ "All new parties, 1%",
+                            name == "np_share_cv_leg" ~ "Genuinely new parties, threshold",
+                            name == "np_share_pnp_leg" ~ "Partially new parties, threshold",
+                            name == "np_share_nc_leg" ~ "All new parties, threshold")) 
+
+app4_data <- np_share_hist_en %>% 
+    group_by(name) %>% 
+    mutate(mean = mean(value)) %>% 
+    ungroup() %>%
+    mutate(name = fct_relevel(name, 
+                              "All new parties, 1%",
+                              "All new parties, threshold",
+                              "Genuinely new parties, 1%",
+                              "Genuinely new parties, threshold",
+                              "Partially new parties, 1%",
+                              "Partially new parties, threshold")) %>% 
+    filter(grepl("threshold", name))
+
+
+(np_share_hist_plot_en_app <-
+        app4_data %>% 
+        ggplot(aes(x = value, group = name))+
+        geom_histogram(bins = 12, breaks = seq(-5,50,5), color = "white")+
+        geom_text(aes(x = mean + 0.06, y = 30, label = sprintf("%1.1f%%", mean)), 
+                  data = app4_data %>% select(country_name_short, mean, name) %>% 
+                      unique()) +
+        geom_vline(aes(xintercept = mean), linetype = "dashed", alpha = 0.5)+
+        scale_x_continuous(labels = label_percent(scale = 1))+
+        scale_y_continuous(minor_breaks = seq(2,10,2), breaks = seq(0,40,10), 
+                           limits = c(0, 60))+
+        labs(x = "Electoral results of new parties",
+             y = "" #, # y = "Počet výskytů v 83 volbách", 
+             #caption = "Historgram je definován takovým způsobem, že první sloupec obsahuje jen volby s nulovým ziskem.\nVertikální přerušovaná čára představuje průměrný volební zisk nových stran.")+
+        )+
+        facet_wrap(~name, ncol = 1)+
+        theme_bw()+
+        theme(legend.position = "none"))
+
+(ch3_all_parties_app <- app4_data %>% 
+        filter(name == "All new parties, threshold") %>% 
+        ggplot(aes(x = value))+
+        geom_histogram(bins = 12, breaks = seq(-5,50,5), color = "white") +
+        geom_text(aes(x = 14.7 + 4, y = 25, label = sprintf("%1.1f%%", 14.7))) +
+        geom_vline(aes(xintercept = mean), linetype = "dashed", alpha = 0.5)+
+        scale_x_continuous(labels = label_percent(scale = 1))+
+        scale_y_continuous(minor_breaks = seq(2,10,2), breaks = seq(0,40,10), 
+                           limits = c(0, 60))+
+        labs(x = "",
+             y = "" #, # y = "Počet výskytů v 83 volbách", 
+             #caption = "Historgram je definován takovým způsobem, že první sloupec obsahuje jen volby s nulovým ziskem.\nVertikální přerušovaná čára představuje průměrný volební zisk nových stran.")+
+        )+
+        facet_wrap(~name) + 
+        theme_bw()+
+        theme(legend.position = "none"))
+
+(ch3_gn_parties_app <- app4_data %>% 
+        filter(name == "Genuinely new parties, threshold") %>% 
+        ggplot(aes(x = value))+
+        geom_histogram(bins = 12, breaks = seq(-5,50,5), color = "white")+
+        geom_text(aes(x = 10.6 + 4, y = 25, label = sprintf("%1.1f%%", 10.6))) +
+        geom_vline(aes(xintercept = mean), linetype = "dashed", alpha = 0.5)+
+        scale_x_continuous(labels = label_percent(scale = 1))+
+        scale_y_continuous(minor_breaks = seq(2,10,2), breaks = seq(0,40,10), 
+                           limits = c(0, 40))+
+        labs(x = "",
+             y = "" #, # y = "Počet výskytů v 83 volbách", 
+             #caption = "Historgram je definován takovým způsobem, že první sloupec obsahuje jen volby s nulovým ziskem.\nVertikální přerušovaná čára představuje průměrný volební zisk nových stran.")+
+        )+
+        facet_wrap(~name) + 
+        theme_bw()+
+        theme(legend.position = "none"))
+
+(ch3_pnp_parties_app <- app4_data %>% 
+        filter(name == "Partially new parties, threshold") %>% 
+        ggplot(aes(x = value))+
+        geom_histogram(bins = 12, breaks = seq(-5,50,5), color = "white") +
+        geom_text(aes(x = 4.4 + 3, y = 30, label = sprintf("%1.1f%%", 4.4))) +
+        geom_vline(aes(xintercept = mean), linetype = "dashed", alpha = 0.5)+
+        scale_x_continuous(labels = label_percent(scale = 1))+
+        scale_y_continuous(breaks = seq(0, 60, 10), minor_breaks = seq(2, 10, 2), 
+                           limits = c(0, 60)) + 
+        labs(x = "Electoral results of new parties",
+             y = "" #, # y = "Počet výskytů v 83 volbách", 
+             #caption = "Historgram je definován takovým způsobem, že první sloupec obsahuje jen volby s nulovým ziskem.\nVertikální přerušovaná čára představuje průměrný volební zisk nových stran.")+
+        )+
+        facet_wrap(~name) + 
+        theme_bw()+
+        theme(legend.position = "none"))
+
+new_parties <- 
+    final_data %>%
+    select(country_name_short, election_year, starts_with("np_"))
+
+new_parties <- 
+    new_parties %>% 
+    select(country_name_short, election_year,starts_with("np_share")) %>% 
+    rename("Genuinely new parties, threshold" = np_share_cv_leg,
+           "Genuinely new parties, 1%" = np_share_cv_1,
+           "All new parties, threshold" = np_share_nc_leg,
+           "All new parties, 1%" = np_share_nc_1,
+           "Partially new parties, threshold" = np_share_pnp_leg,
+           "Partially new parties, 1%" = np_share_pnp_1)
+
+new_party_share <- new_parties %>% 
+    pivot_longer(cols = -c(country_name_short, election_year)) %>% 
+    mutate(name = fct_relevel(name, 
+                              "All new parties, 1%",
+                              "All new parties, threshold",
+                              "Genuinely new parties, 1%",
+                              "Genuinely new parties, threshold",
+                              "Partially new parties, 1%",
+                              "Partially new parties, threshold")) %>% 
+    filter(!is.na(value)) %>% 
+    filter(grepl("threshold", name))
+
+(np_share_facet <- 
+        new_party_share %>% 
+        ggplot(aes(x = election_year, y = value))+
+        geom_point(alpha=0.5)+
+        geom_smooth(se = FALSE, method = "loess", aes(group = 1), linetype = "solid", color = "goldenrod3")+
+        scale_y_continuous(labels = percent_format(accuracy = 1, suffix = " %", scale = 1)) + 
+        scale_x_continuous(breaks = seq(1990, 2020, 2), guide = guide_axis(angle = 90))+
+        labs(x=element_blank(), 
+             y=element_blank(),
+             # caption = "Volební výsledky z databáze Parlgov.org, určení nových stran a výpočty autoři.\nJednotlivá pozorování vyjadřují podíl volebního zisku v jedněch volbách.\nČeské volby vyneseny červeným trojúhelníkem.\nTrendová křivka vytvořena algoritmem LOESS."
+        ) +
+        scale_color_manual(values = c("black", "firebrick2" ))+
+        guides(color = "none") + 
+        facet_rep_wrap(~name, repeat.tick.labels = 'all', ncol = 1)+
+        theme_bw()+
+        theme(panel.spacing = unit(1, "lines"),
+              legend.position = "none"))
+
+(share_all_chart <- new_party_share %>% 
+        filter(name == "All new parties, threshold") %>% 
+        ggplot(aes(x = election_year, y = value))+
+        geom_point(alpha=0.5)+
+        geom_smooth(se = FALSE, method = "loess", aes(group = 1), 
+                    linetype = "solid", color = "goldenrod3")+
+        scale_y_continuous(labels = percent_format(accuracy = 1, suffix = " %", 
+                                                   scale = 1)) + 
+        scale_x_continuous(breaks = seq(1990, 2020, 2), guide = guide_axis(angle = 90))+
+        labs(x=element_blank(), 
+             y=element_blank(),
+             # caption = "Volební výsledky z databáze Parlgov.org, určení nových stran a výpočty autoři.\nJednotlivá pozorování vyjadřují podíl volebního zisku v jedněch volbách.\nČeské volby vyneseny červeným trojúhelníkem.\nTrendová křivka vytvořena algoritmem LOESS."
+        ) +
+        facet_wrap(~name) + 
+        guides(color = "none") + 
+        theme_bw())
+
+(share_gn_chart <- new_party_share %>% 
+        filter(name == "Genuinely new parties, threshold") %>% 
+        ggplot(aes(x = election_year, y = value))+
+        geom_point(alpha=0.5)+
+        geom_smooth(se = FALSE, method = "loess", aes(group = 1), 
+                    linetype = "solid", color = "goldenrod3")+
+        scale_y_continuous(labels = percent_format(accuracy = 1, suffix = " %", 
+                                                   scale = 1)) + 
+        scale_x_continuous(breaks = seq(1990, 2020, 2), guide = guide_axis(angle = 90))+
+        labs(x=element_blank(), 
+             y=element_blank(),
+             # caption = "Volební výsledky z databáze Parlgov.org, určení nových stran a výpočty autoři.\nJednotlivá pozorování vyjadřují podíl volebního zisku v jedněch volbách.\nČeské volby vyneseny červeným trojúhelníkem.\nTrendová křivka vytvořena algoritmem LOESS."
+        ) +
+        facet_wrap(~name) + 
+        guides(color = "none") + 
+        theme_bw())
+
+(share_pnp_chart <- new_party_share %>% 
+        filter(name == "Partially new parties, threshold") %>% 
+        ggplot(aes(x = election_year, y = value))+
+        geom_point(alpha=0.5)+
+        geom_smooth(se = FALSE, method = "loess", aes(group = 1), 
+                    linetype = "solid", color = "goldenrod3")+
+        scale_y_continuous(labels = percent_format(accuracy = 1, suffix = " %", 
+                                                   scale = 1)) + 
+        scale_x_continuous(breaks = seq(1990, 2020, 2), guide = guide_axis(angle = 90))+
+        labs(x=element_blank(), 
+             y=element_blank(),
+             # caption = "Volební výsledky z databáze Parlgov.org, určení nových stran a výpočty autoři.\nJednotlivá pozorování vyjadřují podíl volebního zisku v jedněch volbách.\nČeské volby vyneseny červeným trojúhelníkem.\nTrendová křivka vytvořena algoritmem LOESS."
+        ) +
+        facet_wrap(~name) + 
+        guides(color = "none") + 
+        theme_bw())
+
+(ch3_all_parties_app + share_all_chart) /
+    (ch3_gn_parties_app + share_gn_chart) /
+    (ch3_pnp_parties_app + share_pnp_chart) +
+    plot_annotation(tag_levels = list(c("A", "B")))
+
+ggsave("figs/app_a4A.png",
+       plot = last_plot(),
+       width = 6,
+       height = 7,
+       units = "in",
+       dpi = 300,
+       type = "cairo")
+
+## A4B
+np_number_hist_en <- 
+    final_data %>% 
+    select(country_name_short, election_year, contains("np_number")) %>% 
+    pivot_longer(cols = c(starts_with("np_"))) %>% 
+    filter(!is.na(value)) %>%
+    mutate(name = case_when(name == "np_number_cv_1" ~ "Genuinely new parties, 1%",
+                            name == "np_number_pnp_1" ~ "Partially new parties, 1%",
+                            name == "np_number_nc_1" ~ "All new parties, 1%",
+                            name == "np_number_cv_leg" ~ "Genuinely new parties, threshold",
+                            name == "np_number_pnp_leg" ~ "Partially new parties, threshold",
+                            name == "np_number_nc_leg" ~ "All new parties, threshold")) 
+
+np_number_hist_en %>% 
+    group_by(name) %>% 
+    mutate(mean = mean(value)) %>% 
+    ungroup() %>% 
+    ggplot(aes(x = value, group = name))+
+    geom_histogram(bins = 12, color = "white") + 
+    geom_text(aes(x = mean + 1, y = 30, label = sprintf("%1.2f", mean)),
+              data = . %>% select(name, mean) %>%
+                  unique()) +
+    geom_vline(aes(xintercept = mean), linetype = "dashed", alpha = 0.5)+
+    facet_wrap(~name, ncol = 2) +
+    scale_x_continuous(breaks = seq(0, 12, by = 3))+
+    theme_bw()+
+    labs(x = "Number of new parties",
+         y = "" #, # y = "Počet výskytů v 83 volbách", 
+         #caption = "Historgram je definován takovým způsobem, že první sloupec obsahuje jen volby s nulovým ziskem.\nVertikální přerušovaná čára představuje průměrný volební zisk nových stran.")+
+    )
+    
+ggsave("figs/app_a4B.png",
+       plot = last_plot(),
+       width = 6,
+       height = 7,
+       units = "in",
+       dpi = 300,
+       type = "cairo")
+
+## A4C
+new_parties <- 
+    final_data %>%
+    select(country_name_short, election_year, starts_with("np_"))
+
+new_parties_number <- 
+    new_parties %>% 
+    select(country_name_short, election_year,starts_with("np_number")) %>% 
+    rename("Genuinely new parties, threshold" = np_number_cv_leg,
+           "Genuinely new parties, 1%" = np_number_cv_1,
+           "All new parties, threshold" = np_number_nc_leg,
+           "All new parties, 1%" = np_number_nc_1,
+           "Partially new parties, threshold" = np_number_pnp_leg,
+           "Partially new parties, 1%" = np_number_pnp_1) %>% 
+    pivot_longer(cols = -c(country_name_short, election_year)) %>% 
+    mutate(name = fct_relevel(name, 
+                              "All new parties, 1%",
+                              "All new parties, threshold",
+                              "Genuinely new parties, 1%",
+                              "Genuinely new parties, threshold",
+                              "Partially new parties, 1%",
+                              "Partially new parties, threshold")) %>% 
+    filter(!is.na(value))
+
+new_parties_number %>% 
+    ggplot(aes(x = election_year, y = value))+
+    geom_point(alpha=0.5)+
+    geom_smooth(se = FALSE, method = "loess", aes(group = 1), 
+                linetype = "solid", color = "goldenrod3")+
+    scale_y_continuous(breaks = seq(0, 9, by = 3), 
+                       minor_breaks = 1:12) +
+    scale_x_continuous(breaks = seq(1990, 2020, 2), guide = guide_axis(angle = 90))+
+    labs(x=element_blank(), 
+         y=element_blank(),
+         # caption = "Volební výsledky z databáze Parlgov.org, určení nových stran a výpočty autoři.\nJednotlivá pozorování vyjadřují podíl volebního zisku v jedněch volbách.\nČeské volby vyneseny červeným trojúhelníkem.\nTrendová křivka vytvořena algoritmem LOESS."
+    ) +
+    facet_wrap(~name, ncol = 2) + 
+    guides(color = "none") + 
+    theme_bw()
+
+ggsave("figs/app_a4C.png",
+       plot = last_plot(),
+       width = 6,
+       height = 7,
+       units = "in",
+       dpi = 300,
+       type = "cairo")
+
+## A4D
+new_parties <- 
+    final_data %>%
+    select(country_name_short, election_year, starts_with("np_"))
+
+new_parties_eng <- 
+    new_parties %>% 
+    select(country_name_short, election_year, starts_with("np_number")) %>% 
+    rename("Genuinely new parties, threshold" = np_number_cv_leg,
+           "Genuinely new parties, 1%" = np_number_cv_1,
+           "All new parties, threshold" = np_number_nc_leg,
+           "All new parties, 1%" = np_number_nc_1,
+           "Partially new parties, threshold" = np_number_pnp_leg,
+           "Partially new parties, 1%" = np_number_pnp_1) %>% 
+    mutate(region = case_when(
+        country_name_short %in% c("POL", "EST", "LVA", "SVK", "LTU", "BGR") ~ "'Never stable' region (Baltic states, Bulgaria, Poland, Slovakia)", 
+        country_name_short %in% c("SVN", "CZE", "HRV", "ROU", "HUN") ~ "'Formerly stable' region (Croatia, Czechia, Hungary, Romania, Slovenia)"
+    ))
+
+new_parties_eng %>% 
+    # mutate(is_czech = if_else(country_name_short == "CZE", 1,0)) %>% 
+    pivot_longer(cols = -c(country_name_short, election_year, region)) %>% 
+    mutate(name = fct_relevel(name, 
+                              "All new parties, 1%",
+                              "All new parties, threshold",
+                              "Genuinely new parties, 1%",
+                              "Genuinely new parties, threshold",
+                              "Partially new parties, 1%",
+                              "Partially new parties, threshold")) %>% 
+    filter(!is.na(value)) %>% 
+    ggplot(aes(x = election_year, y = value))+
+    geom_point(alpha=0.5, aes(colour = region))+
+    geom_smooth(se = FALSE, method = "loess", aes(colour = region), linetype = "solid")+
+    scale_y_continuous(breaks = seq(0, 9, by = 3), 
+                       minor_breaks = 1:12) + 
+    scale_x_continuous(breaks = seq(1990, 2020, 2), guide = guide_axis(angle = 90))+
+    scale_colour_viridis_d(option = "D") + 
+    # scale_colour_manual(values = c("#f1a340", "#998ec3")) +
+    labs(x=element_blank(), 
+         y=element_blank(),
+         colour = "") + 
+    facet_rep_wrap(~name, repeat.tick.labels = 'all', ncol = 2)+
+    theme_bw() +
+    theme(panel.spacing = unit(1, "lines"),
+          legend.position = "top") + 
+    guides(colour = guide_legend(nrow=2,byrow=TRUE))
+
+ggsave("figs/app_a4D.png",
+       plot = last_plot(),
+       width = 6,
+       height = 7,
+       units = "in",
+       dpi = 300,
+       type = "cairo")
